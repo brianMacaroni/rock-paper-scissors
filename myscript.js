3c3fd507e1d1553c09b1e449609a0bcc08da6870
Brian Tran <blackbirdmacaroni@gmail.com>
2023-09-12 20:13:02 -0700

    Finish most logic and learned a bit

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
-//declare array of choices and variable to hold robot choice
+//declare array of choices and variables
 const choices = ["rock", "paper", "scissors"];
 let compChoice = getComputerChoice();
-let userChoice = prompt("Choose rock, paper, or scissors: ");
+let userChoice;
 let userPoints = 0;
 let compPoints = 0;
 
@@ -10,64 +10,101 @@ function getComputerChoice() {
   let ye = Math.floor(Math.random() * 3);
   return choices[ye];
 }
+
+let winOrLose;
 //function to compare user choice and robot choice
 function play(playerSelection, computerSelection) {
-  //make user input lowercase to easily compare to robot
-  const input = playerSelection.toLowerCase();
-
+  document.getElementById("userchoice").textContent = playerSelection;
+  document.getElementById("robotchoice").textContent = computerSelection;
   //compare to see if there is a draw
-  if (input == computerSelection) {
-    return `Draw. Your choice: ${input}, and computer choice: ${computerSelection}`;
+  if (playerSelection === computerSelection) {
+    return (winOrLose = "draw");
   }
 
   //compare to see who wins or loses
-  switch (input) {
+  switch (playerSelection) {
     case "rock":
       if (computerSelection == "scissors") {
-        userPoints++;
-        return `You win! ${input} beats ${computerSelection}`;
+        winOrLose = "win";
+        break;
       } else {
-        compPoints++;
-        return `You lose! ${input} loses to ${computerSelection}`;
+        winOrLose = "lose";
+        break;
       }
     case "paper":
       if (computerSelection == "rock") {
-        userPoints++;
-        return `You win! ${input} beats ${computerSelection}`;
+        winOrLose = "win";
+        break;
       } else {
-        compPoints++;
-        return `You lose! ${input} loses to ${computerSelection}`;
+        winOrLose = "lose";
+        break;
       }
     case "scissors":
       if (computerSelection == "paper") {
-        userPoints++;
-        return `You win! ${input} beats ${computerSelection}`;
+        winOrLose = "win";
+        break;
       } else {
-        compPoints++;
-        return `You lose! ${input} loses to ${computerSelection}`;
+        winOrLose = "lose";
+        break;
       }
   }
+  return winOrLose;
 }
-
+let gameEnd;
 //game function
 function game() {
-  let gameEnd = false;
   //loop until user or robot gets 5 points
-  while (gameEnd == false) {
-    console.log(play(userChoice, compChoice));
-    console.log(`User Points: ${userPoints}`);
-    console.log(`Computer Points: ${compPoints}`);
-
-    if (userPoints == 5) {
-      console.log("You beat the cyborg!");
-      return (gameEnd = true);
-    } else if (compPoints == 5) {
-      console.log("The cyborgs take over the world!");
-      return (gameEnd = true);
-    }
-    userChoice = prompt("Choose rock, paper, or scissors: ");
-    compChoice = getComputerChoice();
+  if (winOrLose === "win") {
+    console.log(`You win! ${userChoice} beats ${compChoice}`);
+    userPoints++;
+    document.getElementById("userscore").textContent =
+      "User Score: " + userPoints;
+  } else if (winOrLose === "lose") {
+    console.log(`You lose! ${userChoice} loses to ${compChoice}`);
+    compPoints++;
+    document.getElementById("robotscore").textContent =
+      "Robot Score: " + compPoints;
+  } else {
+    console.log(
+      `You draw! Your Choice: ${userChoice}, Robot Choice: ${compChoice}`
+    );
   }
+
+  if (userPoints === 5) {
+    console.log("You beat the cyborg!");
+    return (gameEnd = true);
+  } else if (compPoints === 5) {
+    console.log("The cyborgs take over the world!");
+    return (gameEnd = true);
+  }
+  console.log("User Points: " + userPoints);
+  console.log("Robot Points: " + compPoints);
+  compChoice = getComputerChoice();
 }
 
-game();
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    if (button.id == "rock") {
+      userChoice = "rock";
+    }
+    if (button.id == "paper") {
+      userChoice = "paper";
+    }
+    if (button.id == "scissors") {
+      userChoice = "scissors";
+    }
+    play(userChoice, compChoice);
+    console.log(userChoice);
+    console.log(compChoice);
+    console.log(winOrLose);
+    game();
+  });
+});
+
+const container = document.querySelector("#container");
+container.setAttribute(
+  "style",
+  "color: blue; background: pink; border-color: black; border-style: solid;"
+);
